5d20e24f0ec820e4213080a9820e9fe1d7385624
Scott Juneau <dsjuneau@gmail.com>
2020-10-06 07:32:24 -0500

    done

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# practice-16
+# Truthy and Falsy
+
+1.  Write code to demonstrate.
+
+## Answer key
+
+Only the following is falsy:
+
+1.  0 and [-0, 0n(BigInt zero)]
+2.  ""
+3.  null
+4.  undefined
+5.  NaN
+6.  false
+
+Everything else is true including:
+
+1.  [] Check for empty by checking length.
+2.  {} Check for empty by looking at keys.
---
+++ app.js
@@ -0,0 +1,60 @@
+let pig;
+let cow = "";
+let horse = null;
+let bird = [];
+let donut = {};
+let num1 = 1;
+let num2 = 0;
+let num3 = -1;
+
+if (num1) {
+  console.log(`${num1} is true.`);
+} else {
+  console.log(`${num1} is false.`);
+}
+if (num2) {
+  console.log(`${num2} is true.`);
+} else {
+  console.log(`${num2} is false.`);
+}
+if (num3) {
+  console.log(`${num3} is true.`);
+} else {
+  console.log(`${num3} is false.`);
+}
+
+if (donut) {
+  console.log(`An empty object is true.`);
+} else {
+  console.log(`An empty object is false.`);
+}
+
+if (bird[0]) {
+  console.log(`${bird[0]} is the first element of an empty array.`);
+} else {
+  console.log(`${bird[0]} is the first element of an empty array.`);
+}
+
+if (bird) {
+  console.log(`An empty array is true.`);
+} else {
+  console.log(`An empty array is false.`);
+}
+
+if (pig) {
+  console.log(`${pig} is true.`);
+} else {
+  console.log(`${pig} is false.`);
+}
+
+if (cow) {
+  console.log(`An empty string is true.`);
+} else {
+  console.log(`An empty string is false.`);
+}
+
+if (horse) {
+  console.log(`${horse} is true.`);
+} else {
+  console.log(`${horse} is false.`);
+}
